33aaa3b03ba5f2a6a5ca8fec3601de13c3b67ca9
Shafiya <shafiyaformcept@gmail.com>
2023-05-17 16:06:17 +0530

    core.clj updated

--- workspace/src/workspace/core.clj
+++ workspace/src/workspace/core.clj
@@ -6,8 +6,8 @@
   [& args]
   (println "Hello World!")
   (println "Welcome to clojure") 
-  (println (+ 1 2)))  
+  (println (+ 1 2))
+  (println "Enter the number")
+  (println (+ 3 4 5)))  
 
-
-heloo formcept
 
